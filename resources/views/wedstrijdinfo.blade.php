94e07c976c7e39937e281600c353a5aec094caa2
robindejonge <jong0259@hz.nl>
2018-03-16 10:54:19 +0100

    wedstrijd invoeren concept

--- resources/views/wedstrijdinfo.blade.php
+++ resources/views/wedstrijdinfo.blade.php
@@ -18,4 +18,31 @@
             <li><a href="#">Team 4</a></li>
         </ul>
     </div>
+        
+    <div id="form">    
+        <form role="form">
+                <div class="box-body">
+                    <div class="form-group">
+                    <label for="matchDate">Datum wedstrijd</label>
+                    <input type="date" class="form-control" id="matchDate" placeholder="voer de wedstrijddatum in">
+                    </div>
+
+                    <div class="form-group">
+                    <label for="teamNameA">Naam team A</label>
+                    <input type="name" class="form-control" id="teamNameA" placeholder="voer de teamnaam van team A in">
+                    </div>
+
+                    <div class="form-group">
+                    <label for="teamNameB">Naam team B</label>
+                    <input type="name" class="form-control" id="teamNameB" placeholder="voer de teamnaam van team B in">
+                    </div>
+                    
+                </div>
+                <!-- /.box-body -->
+
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                </form>
+            </div>
 @stop
