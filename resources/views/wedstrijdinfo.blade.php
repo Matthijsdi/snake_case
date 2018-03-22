b2457f78c24d9458bedbdc6dcaab553efc3ced36
robindejonge <jong0259@hz.nl>
2018-03-22 14:17:58 +0100

    Datum picker gemaakt

--- resources/views/wedstrijdinfo.blade.php
+++ resources/views/wedstrijdinfo.blade.php
@@ -22,16 +22,10 @@
     <div id="form">
         <form role="form">
                 <div class="box-body">
-                    <!-- balk om het ID van de wedstrijd in te vullen -->
+                    <!-- balk om de datum van de wedstrijd in te vullen/te kiezen -->
                     <div class="form-group">
-                        <label for="wedstrijd_id">Wedstrijd ID</label>
-                        <input type="id" class="form-control" id="wedstrijd_id" placeholder="voer het wedstrijdnummer in">
-                    </div>
-
-                    <!-- balk om de datum van de wedstrijd in te vullen -->
-                    <div class="form-group">
-                        <label for="datum">Datum wedstrijd</label>
-                        <input type="date" class="form-control" id="datum" placeholder="voer de datum van de wedstrijd in">
+                        <label for="datum">Datum</label>
+                        <input type="datetime-local" class="form-control" id="datum">
                     </div>
 
                     <!-- balk om de naam van het thuis team in te vullen -->
@@ -52,6 +46,6 @@
                 <div class="box-footer">
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
-                </form>
-            </div>
+            </form>
+        </div>
 @stop
