5519882f3a64516829e9ced8cfe563f9b5a06487
Milan Weug <weug0007@hz.nl>
2018-04-17 13:19:26 +0200

    merge commit

---
+++ resources/views/sets/index.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'MBV Volley')
+
+@section('content_header')
+
+@stop
+
+@section('content')
+<div class="form-group">
+                  <label>Text</label>
+                  <input type="text" class="form-control" placeholder="Enter ...">
+</div>
+
+<div class="form-group">
+                  <label>Text</label>
+                  <input type="text" class="form-control" placeholder="Enter ...">
+</div>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,18 +9,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::resources([
     'home' => 'HomeController',
     'competities' => 'CompetitieController',
     'wedstrijdinfo' => 'WedstrijdInfoController',
     'vereniging' => 'VerenigingController',
-    'kalender' => 'KalenderController'
+    'wedstrijd' => 'WedstrijdController'
     ]);
-
