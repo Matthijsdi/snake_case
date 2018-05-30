32fe14d761a032f70744ff0ae159ca2f4cb0f2dc
Youp Wiggers <wigg0003@hz.nl>
2018-05-30 13:31:50 +0200

    git conflict deel 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
 <div class="box box-primary">
     <div class="box-header with-border">
         <h3 class="box-title">Laatste uitslagen</h3>
-        <p>hello there</p>
+        <p>hello never</p>
 
         <div class="box-tools pull-right">
         <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
